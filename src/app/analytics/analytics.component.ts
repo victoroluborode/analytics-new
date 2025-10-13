7e4a98a60e294347d3d6a8d1b906eb6d33482753
Victor Oluborode <oluborodevictor110@gmail.com>
2025-10-13 14:25:42 +0100

    commit

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -1,4 +1,3 @@
-// analytics.component.ts
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
--- src/app/analytics/analytics.service.ts
+++ src/app/analytics/analytics.service.ts
@@ -1,4 +1,3 @@
-// analytics.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
